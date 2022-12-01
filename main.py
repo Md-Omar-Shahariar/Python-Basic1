bd9aa074c5ea6312dc59001441f2b184b1731cc4
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-01 20:53:07 +0600

    String List

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
-a, b = 0, 1
-while a < 1000:
-    print(a)
-    a, b = b, a + b
-
 # numbers
 d = 2
 c = 3
-s = c+d
+s = c + d
 
-print(d+c)
+print(d + c)
 
 print("Sum : ", s)
 
 print(r"c:ad\newFolder")
 print("c:ad\newFolder")
 
+# Strings
+
+str = "Afridi"
+str = str + "01"
+
+
+print(str[3:])
+print(str[1:3])
+
+# list
+
+list = [1, 2, 6, 4, 5]
+print(list.count(3))
+print(list.pop())
+list.sort()
+print(list)
+list.append(5)
+list.sort()
+print(list)
+list.extend([7, 8, 9, 10])
+print(list)
+list.insert(0, 0)
+print(list)
+list.remove(10)
+print(list)
+print(list.index(9))
+list.reverse()
+print(list)
---
+++ problemSlove.py
@@ -0,0 +1,5 @@
+#fibonacchi series
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
