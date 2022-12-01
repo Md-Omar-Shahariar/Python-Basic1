d05f7ffd9f2f062c0a0f3ba9081d11ff42bc2b7d
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-01 20:24:00 +0600

    Print

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Afridi")
+a, b = 0, 1
+while a < 1000:
+    print(a)
+    a, b = b, a + b
+
+# numbers
+d = 2
+c = 3
+s = c+d
+
+print(d+c)
+
+print("Sum : ", s)
+
+print(r"c:ad\newFolder")
+print("c:ad\newFolder")
+
