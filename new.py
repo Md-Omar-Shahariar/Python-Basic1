3bdc7a65b216060fc72f18578f27a315eaa98782
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-11 00:50:19 +0600

    Import Math

---
+++ new.py
@@ -0,0 +1,16 @@
+#taking input in integer form
+
+x = int(input("Enter an Input : "))
+
+y = int(input("Enter 2nd Input : "))
+z = x+y
+print(z)
+
+# input Char
+ch = input("Enter a  Character")[0]
+print(ch)
+
+## Eval
+
+eval = eval(input("Enter an Expression"))
+print(eval)
