4af63193bc382e6a9c7d0056a4e7c5feb5ef41c1
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-12 17:06:44 +0600

    numpy array

--- np.py
+++ np.py
@@ -1,5 +1,15 @@
 from numpy import *
 
 
-x = array([[1, 2, 3], [4, 5, 0]])
-print(x)
+arr = linspace(0,15,5)
+arr2 = arange(0,15,.1)
+arr3 = logspace(0,15,5)
+arr4 = ones(15)
+print(arr)
+print(arr2)
+print(arr3)
+print(arr4)
+arr5 =array([1,2,3,4,4,5])
+print(unique(arr5))
+print(sort(arr5))
+print(max(arr5))
